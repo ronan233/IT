92f1328efe7d4a665077e62a69754f394a90628c
ronan233 <ronan23353476@outlook.com>
2016-01-09 10:48:21 +0000

    computer name display

---
+++ lib/computername.cmd
@@ -0,0 +1,13 @@
+:: ----------------------------------------------------------------------------
+:: Title : Computer Name
+:: Author: Ronan233
+:: Date: 2016-01-09
+:: Filename : computername.cmd
+:: License : MIT
+:: ----------------------------------------------------------------------------
+:: Description : 
+:: display computer name with CMD command line of the current computer
+:: ----------------------------------------------------------------------------
+@ECHO OFF
+ECHO.%COMPUTERNAME%
+@ECHO ON
